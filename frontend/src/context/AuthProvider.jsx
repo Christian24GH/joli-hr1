d1d134e0a16423b26120f83e725985cb592369aa
Christian24GH <loquezchristian@gmail.com>
2025-10-16 11:54:38 +0800

    fix auth provider

--- frontend/src/context/AuthProvider.jsx
+++ frontend/src/context/AuthProvider.jsx
@@ -9,7 +9,6 @@ export const AuthProvider = ({ children }) => {
   //console.log("AuthProvider using backend: " + env)
   const [auth, setAuth] = useState(null)
   const [loading, setLoading] = useState(true)
-  const navigate = useNavigate()
 
   // always fetch user from backend if cookie exists
   const fetchUser = async () => {
@@ -29,54 +28,6 @@ export const AuthProvider = ({ children }) => {
     fetchUser()
   }, [])
 
-  const login = async (credentials) => {
-    try {
-      await apiLogin(credentials) // cookie set
-      const user = await fetchUser() // now fetch actual user object
-
-      if (user) {
-        toast.success('Login successful, redirecting...', { position: "top-center" })
-        roleAccess(user, navigate)
-      } else {
-        toast.error('Login failed: unable to fetch user', { position: "top-center" })
-      }
-    } catch (error) {
-      if (error.response) {
-        if ([401, 422].includes(error.response.status)) {
-          toast.error('Invalid credentials', { position: "top-center" })
-        } else if (error.response.status === 500) {
-          toast.error('Server error. Please try again later.', { position: "top-center" })
-        } else {
-          toast.error(`Login failed (${error.response.status})`, { position: "top-center" })
-        }
-      } else {
-        toast.error('Network error. Please check your connection.', { position: "top-center" })
-      }
-      console.error(error)
-    }
-  }
-
-  const roleAccess = (user, navigate) => {
-    switch (user?.role) {
-      case 'Super Admin':
-        navigate('/login')
-        break
-      case 'LogisticsII Admin':
-      case 'Driver':
-      case 'Employee':
-        navigate('/logisticsII/')
-        break
-      case 'LogisticsI Admin':
-        navigate('/logistics1/')
-        break
-      case 'HR1 Admin':
-        navigate('/hr1/')
-        break
-      default:
-        navigate('/login')
-    }
-  }
-
   const logout = async () => {
     try {
       await apiLogout()
@@ -84,12 +35,12 @@ export const AuthProvider = ({ children }) => {
       // ignore errors
     } finally {
       setAuth(null)
-      navigate('/login')
+      window.location.href = `${import.meta.env.VITE_LANDING_FRONTEND}/login`
     }
   }
 
   return (
-    <AuthContext.Provider value={{ auth, setAuth, login, loading, roleAccess, logout }}>
+    <AuthContext.Provider value={{ auth, setAuth, loading, logout }}>
       {children}
     </AuthContext.Provider>
   )
